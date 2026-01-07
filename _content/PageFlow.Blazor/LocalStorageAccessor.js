6aac70ea13e88e9fe8f5709b26ed10ea2193a876
Daniel Mendie <66223776+danielmendie@users.noreply.github.com>
2026-01-07 13:58:04 +0100

    rebrand

---
+++ _content/PageFlow.Blazor/LocalStorageAccessor.js
@@ -0,0 +1,16 @@
+﻿export function setItem(key, value) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function getItem(key) {
+    const v = window.localStorage.getItem(key);
+    return v ? JSON.parse(v) : null;
+}
+
+export function removeItem(key) {
+    window.localStorage.removeItem(key);
+}
+
+export function clear() {
+    windo.localStorage.clear();
+}
